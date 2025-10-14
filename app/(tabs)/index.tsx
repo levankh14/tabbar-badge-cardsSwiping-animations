6ffa74181f3d5deef98f990a4a50715166c431ff
levankh14 <levankh14@gmail.com>
2025-10-14 18:08:08 +0700

    boilerPlate code for part 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,44 @@
 import { StyleSheet, Text } from "react-native";
 
 import { TEXT_COLOR } from "@/constants/constants";
+import { Currency } from "@/types/types";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { CirclePoundSterling } from "lucide-react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const ICON_SIZE = {
+  bitcoin: 20,
+  ethereum: 18,
+  solana: 20,
+};
+
+export const currencies: Currency[] = [
+  {
+    currency: "bitcoin",
+    value: "28.02",
+    icon: (
+      <FontAwesome5 name="bitcoin" size={ICON_SIZE.bitcoin} color="#000000" />
+    ),
+    color: "#bdf14d",
+    textColor: "black",
+  },
+  {
+    currency: "ethereum",
+    value: "38.42",
+    icon: (
+      <FontAwesome5 name="ethereum" size={ICON_SIZE.ethereum} color="#000000" />
+    ),
+    color: "black",
+    textColor: "white",
+  },
+  {
+    currency: "solana",
+    value: "10.88",
+    icon: <CirclePoundSterling color="black" size={ICON_SIZE.solana} />,
+    color: "#f14de1ff",
+    textColor: "white",
+  },
+];
 
 export default function TabOneScreen() {
   return (
